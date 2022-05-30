3d8fff50c0bea154c6f3b53c23d380625d83abcc
Mark Adamson <markadamson83@gmail.com>
2022-05-30 15:37:35 +0100

    android: improved event sorting, refreshing, animations

--- admin/android/app/src/main/java/com/markadamson83/pokeventsadmin/ui/EventsScreen.kt
+++ admin/android/app/src/main/java/com/markadamson83/pokeventsadmin/ui/EventsScreen.kt
@@ -1,6 +1,9 @@
 package com.markadamson83.pokeventsadmin.ui
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
@@ -20,10 +23,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.SwipeRefreshState
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.markadamson83.pokeventsadmin.R
-import com.markadamson83.pokeventsadmin.domain.Event
 import com.markadamson83.pokeventsadmin.domain.Region
 import com.markadamson83.pokeventsadmin.presentation.MainViewModel
 import com.markadamson83.pokeventsadmin.presentation.Status
@@ -32,6 +33,7 @@ import com.markadamson83.pokeventsadmin.ui.theme.*
 import java.text.SimpleDateFormat
 import java.util.*
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun EventsScreen(viewModel: MainViewModel, navController: NavController) {
     val events = viewModel.events.observeAsState()
@@ -47,11 +49,18 @@ fun EventsScreen(viewModel: MainViewModel, navController: NavController) {
                 })
         },
         floatingActionButton = {
-            FloatingActionButton(
-                modifier = Modifier.padding(24.dp),
-                onClick = { navController.navigate(Screen.EventScreen.route) },
+            val fabVisible by derivedStateOf { events.value?.status == Status.SUCCESS }
+            AnimatedVisibility(
+                visible = fabVisible,
+                enter = slideInVertically(initialOffsetY = { fullHeight -> fullHeight }),
+                exit = slideOutVertically(targetOffsetY = { fullHeight -> fullHeight })
             ) {
-                Icon(imageVector = Icons.Default.Add, "")
+                FloatingActionButton(
+                    modifier = Modifier.padding(24.dp),
+                    onClick = { navController.navigate(Screen.EventScreen.route) },
+                ) {
+                    Icon(imageVector = Icons.Default.Add, "")
+                }
             }
         }
     ) { paddingValues ->
@@ -60,133 +69,119 @@ fun EventsScreen(viewModel: MainViewModel, navController: NavController) {
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
-            val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = false)
+            val isRefreshing by derivedStateOf { listOf(null, Status.NONE, Status.LOADING).contains(events.value?.status) }
 
-            if (swipeRefreshState.isRefreshing) {
-                if (events.value?.message != null)
-                    ErrorContent(viewModel, swipeRefreshState, events.value?.message)
-                else if (events.value?.value != null)
-                    EventsContent(viewModel, navController, swipeRefreshState, events)
-            } else
+            SwipeRefresh(
+                state = rememberSwipeRefreshState (isRefreshing),
+                onRefresh = { viewModel.getEvents() }
+            ) {
                 when (events.value?.status) {
-                    null, Status.NONE, Status.LOADING ->
-                        CircularProgressIndicator(Modifier.align(Alignment.Center))
                     Status.ERROR ->
-                        ErrorContent(viewModel, swipeRefreshState, events.value?.message)
-                    Status.SUCCESS ->
-                        EventsContent(viewModel, navController, swipeRefreshState, events)
-                }
-        }
-    }
-}
-
-
-@Composable
-internal fun ErrorContent(viewModel: MainViewModel, swipeRefreshState: SwipeRefreshState, error: String?) {
-    SwipeRefresh(
-        state = swipeRefreshState, // rememberSwipeRefreshState(events.value!!.status == Status.LOADING),
-        onRefresh = { viewModel.getEvents() }
-    ) {
-        Box(
-            Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())) {
-            error?.let { msg ->
-                Text(
-                    msg,
-                    Modifier.align(Alignment.Center)
-                )
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-internal fun EventsContent(viewModel: MainViewModel, navController: NavController, swipeRefreshState: SwipeRefreshState,
-                           events: State<com.markadamson83.pokeventsadmin.presentation.Result<Map<String, Event>>?>) {
-    val colourMap = remember { EnumMap(Region.values().associateWith {
-        when (it!!) {
-            Region.WORLDWIDE -> Worldwide
-            Region.AMERICA -> America
-            Region.JAPAN -> Japan
-            Region.EUROPE -> Europe
-            Region.OTHER -> Other
-        }
-    })}
-
-    var deleting: String? by remember { mutableStateOf(null) }
-
-    deleting?.let {
-        AlertDialog(
-            onDismissRequest = { deleting = null },
-            title = { Text("Delete Event?") },
-            text = { Text("Are you sure you want to delete the event \"${events.value!!.value!![it]?.title}\"?") },
-            confirmButton = {
-                TextButton(onClick = {
-                    viewModel.deleteEvent(it)
-                    deleting = null
-                }) {
-                    Text(stringResource(R.string.yes))
-                }
-            },
-            dismissButton = {
-                TextButton(onClick = {
-                    deleting = null
-                }) {
-                    Text(stringResource(R.string.no))
-                }
-            }
-        )
-    }
-
-    val sortedEvents = remember(events) {
-        events.value?.value?.entries?.sortedBy { it.value.startDate }?.groupBy { it.value.region }
-    }
-
-    SwipeRefresh(
-        state = swipeRefreshState, // rememberSwipeRefreshState(events.value!!.status == Status.LOADING),
-        onRefresh = { viewModel.getEvents() }
-    ) {
-        LazyColumn(
-            Modifier
-                .fillMaxSize()
-        ) {
-
-            sortedEvents?.let { events ->
-                events.keys.forEach{ region ->
-                    stickyHeader {
-                        Surface(
-                            Modifier.fillMaxWidth(),
-                            color = colourMap[region]!!
+                        Box(
+                            Modifier
+                                .fillMaxSize()
+                                .verticalScroll(rememberScrollState())
                         ) {
-                            Text(region.label,
-                                modifier = Modifier.padding(4.dp),
-                                style = MaterialTheme.typography.caption)
+                            events.value?.message?.let { msg ->
+                                Text(
+                                    msg,
+                                    Modifier.align(Alignment.Center)
+                                )
+                            }
                         }
-                    }
+                    Status.SUCCESS -> {
+                        val colourMap = remember { EnumMap(Region.values().associateWith {
+                            when (it!!) {
+                                Region.WORLDWIDE -> Worldwide
+                                Region.AMERICA -> America
+                                Region.JAPAN -> Japan
+                                Region.EUROPE -> Europe
+                                Region.OTHER -> Other
+                            }
+                        })}
 
-                    items(events[region]!!) { event ->
-                        Column(
-                            Modifier.combinedClickable(
-                                onClick = {
-                                    navController.navigate(Screen.EventScreen.withArgs(event.key))
+                        var deleting: String? by remember { mutableStateOf(null) }
+
+                        deleting?.let {
+                            AlertDialog(
+                                onDismissRequest = { deleting = null },
+                                title = { Text("Delete Event?") },
+                                text = { Text("Are you sure you want to delete the event \"${events.value!!.value!![it]?.title}\"?") },
+                                confirmButton = {
+                                    TextButton(onClick = {
+                                        viewModel.deleteEvent(it)
+                                        deleting = null
+                                    }) {
+                                        Text(stringResource(R.string.yes))
+                                    }
                                 },
-                                onLongClick = {
-                                    deleting = event.key
+                                dismissButton = {
+                                    TextButton(onClick = {
+                                        deleting = null
+                                    }) {
+                                        Text(stringResource(R.string.no))
+                                    }
                                 }
                             )
+                        }
+
+                        val sortedEvents = remember(events) {
+                            events.value?.value?.entries?.sortedBy { it.value.startDate }?.groupBy { it.value.region }?.toSortedMap()
+                        }
+
+                        LazyColumn(
+                            Modifier
+                                .fillMaxSize()
                         ) {
-                            Row(Modifier.padding(16.dp), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-                                DateDisplay(event.value.startDate)
-                                Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                                    Text(event.value.title)
-                                    Text(event.value.games.joinToString("/") { it.label }, style = MaterialTheme.typography.caption)
+                            sortedEvents?.let { events ->
+                                events.keys.forEach{ region ->
+                                    stickyHeader {
+                                        Surface(
+                                            Modifier.fillMaxWidth(),
+                                            color = colourMap[region]!!
+                                        ) {
+                                            Text(region.label,
+                                                modifier = Modifier.padding(4.dp),
+                                                style = MaterialTheme.typography.caption)
+                                        }
+                                    }
+
+                                    items(
+                                        items = events[region]!!,
+                                        key = {
+                                            it.key
+                                        }
+                                    ) { event ->
+                                        Column(
+                                            Modifier.combinedClickable(
+                                                onClick = {
+                                                    navController.navigate(Screen.EventScreen.withArgs(event.key))
+                                                },
+                                                onLongClick = {
+                                                    deleting = event.key
+                                                }
+                                            ).animateItemPlacement()
+                                        ) {
+                                            Row(Modifier.padding(16.dp), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                                                DateDisplay(event.value.startDate)
+                                                Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                                                    Text(event.value.title)
+                                                    Text(event.value.games.joinToString("/") { it.label }, style = MaterialTheme.typography.caption)
+                                                }
+                                            }
+                                            Divider()
+                                        }
+                                    }
                                 }
                             }
-                            Divider()
                         }
                     }
+                    else ->
+                        Box(
+                            Modifier
+                                .fillMaxSize()
+                                .verticalScroll(rememberScrollState())
+                        ) {}
                 }
             }
         }
